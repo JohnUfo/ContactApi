fdc0d81be4f90d7248e4a1ea95c9b2eb2bf0f6a4
JohnUfo <tursunov.umidjon.uz@gmail.com>
2025-02-03 16:49:55 +0500

    entity and repository and service and resource is created for contact

---
+++ src/main/java/online/muydinov/contactapi/resouorce/ContactResource.java
@@ -0,0 +1,51 @@
+package online.muydinov.contactapi.resouorce;
+
+import lombok.RequiredArgsConstructor;
+import online.muydinov.contactapi.domain.Contact;
+import online.muydinov.contactapi.service.ContactService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static online.muydinov.contactapi.constant.Constant.PHOTO_DIRECTORY;
+
+@RestController
+@RequestMapping("/contacts")
+@RequiredArgsConstructor
+public class ContactResource {
+
+    private final ContactService contactService;
+
+    @PostMapping
+    public ResponseEntity<Contact> createContact(@RequestBody Contact contact) {
+        return ResponseEntity.created(URI.create("/contacts/userID")).body(contactService.createContact(contact));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Contact>> getContacts(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                     @RequestParam(value = "size", defaultValue = "10") int size) {
+        return ResponseEntity.ok().body(contactService.getALlContacts(page, size));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Contact> getContacts(@PathVariable(value = "id") String id) {
+        return ResponseEntity.ok().body(contactService.getContact(id));
+    }
+
+    @PutMapping("/photo")
+    public ResponseEntity<String> uploadPhoto(@RequestParam("id") String id, @RequestParam("file") MultipartFile file) throws IOException {
+        return ResponseEntity.ok().body(contactService.uploadPhoto(id, file));
+    }
+
+    @GetMapping(path = "/image/{filename}")
+    public byte[] getPhoto(@PathVariable String filename) throws IOException {
+        return Files.readAllBytes(Paths.get(PHOTO_DIRECTORY + filename));
+    }
+
+}
---
+++ src/main/java/online/muydinov/contactapi/service/ContactService.java
@@ -0,0 +1,75 @@
+package online.muydinov.contactapi.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import online.muydinov.contactapi.domain.Contact;
+import online.muydinov.contactapi.repo.ContactRepo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static online.muydinov.contactapi.constant.Constant.PHOTO_DIRECTORY;
+
+@Service
+@Slf4j
+@Transactional(rollbackOn = Exception.class)
+@RequiredArgsConstructor
+public class ContactService {
+    private final ContactRepo contactRepo;
+
+    public Page<Contact> getALlContacts(int page, int size) {
+        return contactRepo.findAll(PageRequest.of(page, size, Sort.by("name")));
+    }
+
+    public Contact getContact(String id) {
+        return contactRepo.findById(id).orElseThrow(() -> new RuntimeException("Contact not found"));
+    }
+
+    public Contact createContact(Contact contact) {
+        return contactRepo.save(contact);
+    }
+
+    public void deleteContact(String id) {
+//        return contactRepo.deleteById(id);
+    }
+
+    public String uploadPhoto(String id, MultipartFile file) {
+        log.info("Saving picture for user Id : {}", id);
+        Contact contact = getContact(id);
+        String photoUrl = photoFunction.apply(id, file);
+        contact.setPhotoUrl(photoUrl);
+        contactRepo.save(contact);
+        return photoUrl;
+    }
+
+    private final Function<String, String> fileExtension = filename -> Optional.of(filename).filter(name -> name.contains("."))
+            .map(name -> "." + name.substring(filename.lastIndexOf(".") + 1)).orElse(".png");
+
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        String fileName = id + fileExtension.apply(image.getOriginalFilename());
+        try {
+            Path fileStorageLocation = Paths.get(PHOTO_DIRECTORY).toAbsolutePath().normalize();
+            if (!Files.exists(fileStorageLocation)) {
+                Files.createDirectories(fileStorageLocation);
+            }
+            Files.copy(image.getInputStream(), fileStorageLocation.resolve(fileName), REPLACE_EXISTING);
+            return ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/contacts/image/" + fileName).toUriString();
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to save image");
+        }
+    };
+}
